394630a7c762f34decc4902154877e4b4d0bcc74
Katos <7927609+katosdev@users.noreply.github.com>
2021-07-11 13:44:48 +0100

    Create Reply2See extension

---
+++ README.md
@@ -0,0 +1,17 @@
+# Reply 2 See
+Support me: 
+https://www.buymeacoffee.com/katos // https://paypal.me/pools/c/8B42aPPAxP
+
+### Installation
+
+```sh
+composer require katosdev/reply2see
+```
+
+### Updating
+
+```sh
+composer update katosdev/reply2see
+php flarum cache:clear
+```
+
---
+++ extend.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of katosdev/reply2see.
+ *
+ * Copyright (c) 2021
+ * Updated by KatosDev
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace katosdev\Reply2See;
+
+use Flarum\Extend;
+use Flarum\Extend\Formatter;
+use Illuminate\Contracts\Events\Dispatcher;
+use s9e\TextFormatter\Configurator;
+
+return [
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/resources/less/forum.less'),
+    (new Extend\Formatter)
+	    ->configure(function (Configurator $config) {
+	        $config->BBcodes->addCustom(
+	            '[REPLY]{TEXT}[/REPLY]',
+	            '<reply2see>{TEXT}</reply2see>'
+	        );
+   		 }),
+    new Extend\Locales(__DIR__ . '/resources/locale'),
+	function (Dispatcher $events){
+	    $events->subscribe(Listeners\LoadSettingsFromDatabase::class);
+	}
+	
+
+];
